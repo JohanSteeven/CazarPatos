dfc286f54280a26cb4fada2f00609f45bc8489d5
P3E004-K <P3E004-K@DESKTOP-27OO8SM>
2025-05-30 15:06:46 -0500

    Main Activity

---
+++ app/src/main/java/com/bano/johan/cazarpatos/Constants.kt
@@ -0,0 +1,3 @@
+package com.bano.johan.cazarpatos
+
+const val EXTRA_LOGIN = "EXTRA_LOGIN"
--- app/src/main/java/com/bano/johan/cazarpatos/MainActivity.kt
+++ app/src/main/java/com/bano/johan/cazarpatos/MainActivity.kt
@@ -1,8 +1,6 @@
 package com.bano.johan.cazarpatos
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -13,17 +11,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-
-        val buttonLogin = findViewById<Button>(R.id.buttonLogin)
-        buttonLogin.setOnClickListener {
-            val intent = Intent(this, MainActivity2::class.java)
-            startActivity(intent)
         }
     }
 }
